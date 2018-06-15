0275d16c688fc8816e3d2d6a5bedb980ca712105
pirsig78 <pirsig@rambler.ru>
2018-06-16 01:23:51 +0300

    Lessons arrays commit 1

---
+++ src/arrayexample/ArrayExamples.java
@@ -0,0 +1,53 @@
+
+package arrayexample;
+
+//import javax.swing.JOptionPane;
+
+public class ArrayExamples {
+    public static void main(String[] args) {
+    //int n = Integer.parseInt(JOptionPane.showInputDialog("Строки: "));
+    //int m = Integer.parseInt(JOptionPane.showInputDialog("Столбцы: "));
+    int n = (int)(5*Math.random()+2);
+    int array [][] = new int [n][];
+    int i;
+    int j;
+    int k;
+    int c ;
+    for (i=0; i<n; i++){
+        
+        k = (int)(5*Math.random()+2);
+        array[i] = new int [k];
+        for (j=0; j<k ; j++){
+            array[i][j] = (int)(11*Math.random()+1);
+            String text = array[i][j]<10 ? "   " : "  ";
+            System.out.print (array[i][j] + text);
+        }
+        System.out.println();
+    }
+    System.out.println();
+    
+    for (i=0; i<array.length; i++){
+        for (j=0; j<array[i].length; j++ ){
+            for (n=i; n<array.length; n++){
+                if(n==i)c=j;
+                    else c=0;
+                for ( int m = c ; m<array[n].length; m++ ){
+                    
+                    if (array[i][j]>array[n][m]){
+                        int z = array[i][j];
+                        array[i][j] = array[n][m];
+                        array[n][m] = z;     
+                    }
+                   
+                }
+            }
+            String text = array[i][j]<10 ? "   " : "  ";
+            System.out.print (array[i][j] + text);
+        }
+      System.out.println();  
+    }
+
+     
+   }
+}
+    
